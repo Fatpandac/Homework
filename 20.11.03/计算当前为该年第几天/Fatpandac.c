9e9062ff524c47dac0816a7126099c2a63c04eba
Fatpandac <1779196284@qq.com>
2021-05-24 19:31:29 +0800

    Optimization

--- 20.11.03/计算当前为该年第几天/Fatpandac.c
+++ 20.11.03/计算当前为该年第几天/Fatpandac.c
@@ -5,19 +5,6 @@ int main()
 	int year, month, day;
 	printf("Input year, month, day:");
 	scanf("%d %d %d", &year, &month, &day);
-	if (year % 4 == 0)
-	{
-		year = 1;
-	}
-	else
-	{
-		year = 0;
-	}
-	switch (year)
-	{
-		case 0: day = (month - 1) * 31 - (month / 2 - 1) - 3 + day;printf("Days of year: %d", day);break;
-		case 1: day = (month - 1) * 31 - (month / 2 - 1) - 2 + day;printf("Days of year: %d", day);break;
-		default:break;
-	}
+	printf("Days of year: %d",(month - 1) * 31 - (month / 2 - 1) - (3 - (year % 4 == 0)) + day);
 	return 0;
 }
